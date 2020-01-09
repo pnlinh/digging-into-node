9c647ef726775b828771d9d91b63bc1d7417375e
Ngoc Linh Pham <pnlinh1207@gmail.com>
2020-01-09 15:40:04 +0700

    reading file

---
+++ ex1.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var fs = require('fs');
+var path = require('path');
+var minimist = require('minimist');
+
+var args = minimist(process.argv.slice(2), {
+    boolean: ['help'],
+    string: ['file']
+});
+
+if (args.help) {
+    printHelp();
+} else if (args.file) {
+    processFile(path.resolve(args.file))
+} else {
+    error('Incorrect usage.');
+}
+
+function processFile(filepath) {
+    var contents = fs.readFileSync(filepath, 'utf8');
+
+    process.stdout.write(contents);
+}
+
+function error(msg, includeHelp = true) {
+    console.error(msg);
+
+    if (includeHelp) {
+        console.log('');
+        printHelp();
+    }
+}
+
+function printHelp() {
+    console.log('ex1 usage:');
+    console.log('   ex1.js --file={FILENAME}');
+    console.log('');
+    console.log('--help            print this help');
+    console.log('--file={FILENAME} process the file');
+    console.log('');
+}
