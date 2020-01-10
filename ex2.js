6a68726b379dc79eb9223183ec4dcfcf41056119
Ngoc Linh Pham <pnlinh1207@gmail.com>
2020-01-10 10:56:13 +0700

    file handle with stream

---
+++ ex2.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var fs = require('fs');
+var path = require('path');
+var minimist = require('minimist');
+var util = require('util');
+var getStdin = require('get-stdin');
+
+var BASE_PATH = path.resolve(
+    process.env.BASE_PATH || __dirname
+);
+
+var args = minimist(process.argv.slice(2), {
+    boolean: ['help', 'in'],
+    string: ['file']
+});
+
+if (args.help) {
+    printHelp();
+} else if (
+    args.in ||
+    args._.includes('-')
+) {
+    processFile(process.stdin);
+} else if (args.file) {
+    let stream = fs.createReadStream(path.join(BASE_PATH, args.file));
+    processFile(stream);
+} else {
+    error('Incorrect usage.');
+}
+
+function processFile(inStream) {
+    var outStream = inStream;
+    var targetStream = process.stdout;
+    outStream.pipe(targetStream);
+}
+
+function error(msg, includeHelp = true) {
+    console.error(msg);
+
+    if (includeHelp) {
+        console.log('');
+        printHelp();
+    }
+}
+
+function printHelp() {
+    console.log('ex1 usage:');
+    console.log('   ex1.js --file={FILENAME}');
+    console.log('');
+    console.log('--help            print this help');
+    console.log('--file={FILENAME} process the file');
+    console.log('--in, -          process stdin');
+    console.log('');
+}
